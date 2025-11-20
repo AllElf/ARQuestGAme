e27d28bbfa1d12e57675db2cdeb05e3762483cde
AllElf <allelf26944@yandex.ru>
2025-11-20 12:01:34 +0500

    commit

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "DefaultCompany-AR-0.1";
 const contentToCache = [
     "Build/4a4635c34ce8c80d318c4a54986bc11b.loader.js",
     "Build/4cbb4bd83ac3cac0ebb8b3cf83aa9410.framework.js.unityweb",
-    "Build/9751fd7ef72d2852f86c19c5183f6acb.data.unityweb",
+    "Build/946a2107ab0314ffe3abe2859c6522cf.data.unityweb",
     "Build/7484325a5a28b0709fddb266d58fb157.wasm.unityweb",
     "TemplateData/style.css"
 
